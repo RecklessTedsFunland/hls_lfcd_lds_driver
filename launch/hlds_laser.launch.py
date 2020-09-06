6041f8619b7e3183f634f97bfa5932a76bf651f9
Kevin <walchko@users.noreply.github.com>
2020-09-06 09:05:00 -0400

    Update hlds_laser.launch.py

--- launch/hlds_laser.launch.py
+++ launch/hlds_laser.launch.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Darby Lim, Pyo
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -29,7 +12,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     port = LaunchConfiguration('port', default='/dev/ttyUSB0')
 
-    frame_id = LaunchConfiguration('frame_id', default='laser')
+    frame_id = LaunchConfiguration('frame_id', default='laser_frame')
+    
+    tf2_node = Node(package='tf2_ros',
+                    executable='static_transform_publisher',
+                    name='static_tf_pub_laser',
+                    arguments=['0.0', '0.0', '0.0','0', '0', '0', '1','base_link','laser_frame'], # x, y, z, yaw, pitch, roll
+                    )
 
     return LaunchDescription([
 
@@ -41,12 +30,14 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'frame_id',
             default_value=frame_id,
-            description='Specifying frame_id of lidar. Default frame_id is \'laser\''),
+            description='Specifying frame_id of lidar. Default frame_id is \'laser_frame\''),
 
         Node(
-            package='hls_lfcd_lds_driver',
-            node_executable='hlds_laser_publisher',
-            node_name='hlds_laser_publisher',
+            package='rtf_lds_driver',
+            executable='hlds_laser_publisher',
+            name='hlds_laser_publisher',
             parameters=[{'port': port, 'frame_id': frame_id}],
             output='screen'),
+        
+        tf2_node,
     ])
